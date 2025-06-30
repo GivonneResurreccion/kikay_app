e922e7eebb52ca9321d19462991988afd3e7c262
Rei Djemf <itsrrei@gmail.com>
2025-07-01 02:05:05 +0800

    Kikay splash

---
+++ lib/routes.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+//Screens folder
+import 'package:kikay/screens/splash.dart';
+
+final Map<String, WidgetBuilder> appRoutes = {
+  '/': (context) => SplashPage(
+    onComplete: () => Navigator.pushNamed(context, '/home'),
+  ),
+
+  // Add this for the target screen after splash
+  //'/home': (context) => homePage(), // replace with your actual screen widget
+};
