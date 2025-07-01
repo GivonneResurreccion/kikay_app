08a4dae08cf72a65dfbf2023146b0adcf467508f
Givonne Resurreccion <resurrecciongivonne.04@gmail.com>
2025-07-01 08:47:15 +0800

    Fixed the emulator on my end

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,12 @@ allprojects {
     }
 }
 
+plugins.withType<com.android.build.gradle.BasePlugin>().configureEach {
+    configure<com.android.build.gradle.BaseExtension> {
+        ndkVersion = "27.0.12077973"
+    }
+}
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
@@ -12,6 +18,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
